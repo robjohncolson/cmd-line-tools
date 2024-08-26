d47e2e7a4a4e34f144ed1c9ffd86b8bd8b750fb6
robert colson <robjohncolson@protonmail.com>
2024-08-26 19:52:19 -0400

    closing up the toolbox for the night

---
+++ tools.bat
@@ -0,0 +1,40 @@
+@echo off
+setlocal
+
+:: Confirm username
+::echo Current username: %USERNAME%
+::set /p confirm="Is this correct? (Y/N): "
+::if /i "%confirm%" neq "Y" (
+::    set /p USERNAME="Enter correct username: "
+::)
+
+
+REM find path to cmd-line-tools
+
+set "cmd-line-tools="
+for /f "delims=" %%i in ('dir /s /b /ad "%USERPROFILE%\cmd-line-tools" 2^>nul') do (
+    if not defined cmd-line-tools (
+        set "cmd-line-tools=%%~dpi"
+        REM echo cmd-line-tools folder found at: %%~dpi
+        REM echo File: %%~nxi
+        REM echo Path: %%~dpi
+        REM echo File+Path: %%i
+    )
+)
+
+if not defined cmd-line-tools (
+    echo cmd-line-tools folder not found in user's directory.
+)
+
+echo $env:PATH += ";%cmd-line-tools%" | clip
+
+if defined cmd-line-tools (
+    echo Contents of cmd-line-tools directory:
+    for /f "delims=" %%a in ('dir /b "%cmd-line-tools%"') do (
+        echo %%a
+    )
+) else (
+    echo cmd-line-tools directory not found or path not set.
+)
+endlocal
+
